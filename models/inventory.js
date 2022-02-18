f95d44f922d15f862636a906d5151a1288924469
aeyerly <83253088+aeyerly@users.noreply.github.com>
2022-02-17 22:37:41 -0500

    Create inventory.js

---
+++ models/inventory.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('user_item', {
+        user_id: DataTypes.INTEGER,
+        item_id: DataTypes.INTEGER,
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+
+    }, {
+        timestamps: false,
+    });
+};
